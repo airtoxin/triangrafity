b252339bff877239b0ff263df325c2d5b2a01e0a
airtoxin <airtoxin@icloud.com>
2016-04-25 08:11:50 +0900

    Use random palette

--- src/App/SettingGroup/Color.jsx
+++ src/App/SettingGroup/Color.jsx
@@ -1,19 +1,50 @@
 import React, {Component} from "react";
 import {branch} from "baobab-react/higher-order";
-import ColorsPicker from "react-colors-picker";
+import palettes from "./palettes";
 import actions from "../../actions";
 
 export class Color extends Component {
+    constructor(prop) {
+        super(prop)
+        this.palettes = palettes;
+        this.state = {
+            paletteIndex: Math.floor(Math.random() * palettes.length),
+            paintingColorIndex: 0
+        };
+    }
+
+    componentDidMount() {
+        const color = this.palettes[this.state.paletteIndex].colors[this.state.paintingColorIndex];
+        this.props.actions.setPaletColor(color);
+    }
+
     render() {
+        const colors = this.palettes[this.state.paletteIndex].colors.map((color, i) => {
+            return (
+                <div
+                    key={i}
+                    style={{
+                        width: "20px",
+                        height: "20px",
+                        margin: "5px",
+                        backgroundColor: color,
+                        border: this.state.paintingColorIndex === i ? "solid 1px red" : "none"
+                    }}
+                    onClick={() => this.handleClick(color, i)}
+                ></div>
+            );
+        });
+
         return (
             <div>
                 <p>Painting color</p>
-                <ColorsPicker color={this.props.color} onChange={(e) => this.handleChange(e.color)}/>
+                <div style={{display: "flex"}}>{colors}</div>
             </div>
         );
     }
 
-    handleChange(color) {
+    handleClick(color, i) {
+        this.setState({paintingColorIndex: i});
         this.props.actions.setPaletColor(color);
     }
 }
