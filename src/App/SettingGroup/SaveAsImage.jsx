869f35c0944123db6c7850d23284fea3536fc42e
airtoxin <airtoxin@icloud.com>
2016-05-02 10:35:20 +0900

    Use button component

--- src/App/SettingGroup/SaveAsImage.jsx
+++ src/App/SettingGroup/SaveAsImage.jsx
@@ -1,11 +1,16 @@
 import React, {Component} from "react";
 import {branch} from "baobab-react/higher-order";
+import {Button} from "rebass";
 import {saveSvgAsPng} from "save-svg-as-png";
 
 export default class SaveAsImage extends Component {
     render() {
         return (
-            <button onClick={this.handleClick.bind(this)}>Save as png</button>
+            <Button
+                onClick={this.handleClick.bind(this)}
+                rounded={true}
+                big={false}
+            >Save as png</Button>
         );
     }
 
