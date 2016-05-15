89dc393e9f48dc6da4cf41806355eca6db7e6e28
airtoxin <airtoxin@icloud.com>
2016-05-15 13:25:48 +0900

    why-did-you-update

--- src/index.jsx
+++ src/index.jsx
@@ -1,11 +1,16 @@
 import React from "react";
 import injectTapEventPlugin from "react-tap-event-plugin";
 import ReactDom from "react-dom";
+import {whyDidYouUpdate} from "why-did-you-update";
 import {root} from "baobab-react/higher-order";
 import tree from "./state";
 import App from "./App/index.jsx";
 
 injectTapEventPlugin();
+
+if (process.env.NODE_ENV !== "production") {
+    whyDidYouUpdate(React);
+}
 
 const Rooted = root(tree, App);
 ReactDom.render(
